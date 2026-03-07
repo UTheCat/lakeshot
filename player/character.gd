d3f769cfe363b871675b924000a0e13640c0a428
UTheCat <bees9633@gmail.com>
2026-03-06 19:32:53 -0800

    more work on character physics

--- player/character.gd
+++ player/character.gd
@@ -32,10 +32,79 @@ var try_jump: bool = false
 ## Whether or not the character wants to climb something
 var try_climb: bool = false
 
+var _climb_hitbox: Area3D = null
+var _climb_direction_cast: ShapeCast3D = null
+var _step_climb_cast: ShapeCast3D = null
+
+var _last_xz_velocity: Vector3 = Vector3.ZERO
+
+# The "vertical" velocity based on the gravity vector
+# Needs to be stored separately from other velocities because
+# gravity vector is not necessarily (0, -1, 0)
+var _last_vertical_velocity: Vector3 = Vector3.ZERO
+
 # Private for now to support future implementation of different acceleration values based on
 # surface material
 const _ACCELERATION = 600
 const _AIR_ACCELERATION = 400
+
+const _VELOCITY_DIFF_SNAP_THRESHOLD = 0.1
+	
+func _on_ready():
+	_climb_hitbox = get_node("ClimbHitbox")
+	_climb_direction_cast = get_node("ClimbDirectionCast")
+	_step_climb_cast = get_node("StepClimbCast")
+
+func _physics_process(delta: float) -> void:
+	var last_xz_velocity = _last_xz_velocity
+	var last_vertical_velocity = _last_vertical_velocity
+	
+	var c_is_on_floor = is_on_floor() # Are we currently on the floor?
+	var gravity = get_gravity()
+	var gravity_direction = gravity.normalized()
+	
+	# How much the character's velocity will change this physics frame
+	#var velocity_delta = Vector3.ZERO
+	
+	var is_trying_to_move = is_trying_to_move();
+	var move_direction = get_move_direction(0)
+	var accel = _ACCELERATION if c_is_on_floor else _AIR_ACCELERATION
+	var next_xz_velocity = _approach_xz_velocity(Vector2(last_xz_velocity.x, last_xz_velocity.z), Vector2(move_direction.x, move_direction.z), accel, delta)
+	var next_vertical_velocity = last_vertical_velocity
+	
+	if try_jump:
+		if c_is_on_floor or (try_climb and can_climb()):
+			# Jump velocity is specified this way to support different gravity directions
+			next_vertical_velocity = -gravity_direction * jump_velocity * delta
+		else:
+			next_vertical_velocity += gravity_direction * delta
+	elif can_climb():
+		var climb_velocity = 0
+		var can_apply_climb_velocity = false
+		
+		if is_trying_to_move:
+			pass
+		else:
+			climb_velocity = 0
+	elif not c_is_on_floor:
+		next_vertical_velocity += gravity_direction * delta
+	
+	_last_xz_velocity = next_xz_velocity
+	_last_vertical_velocity = next_vertical_velocity
+	
+	velocity = next_xz_velocity + next_vertical_velocity
+	
+func _approach_xz_velocity(current_vel: Vector2, goal_vel: Vector2, accel: float, physics_delta_time: float):
+	var direction = (goal_vel - current_vel).normalized()
+	var xz_velocity_delta = direction * accel * physics_delta_time
+	var final_vel = current_vel + xz_velocity_delta
+	
+	# To prevent unwanted movement due to floating-point precision errors
+	var new_xz_vel_diff = goal_vel - final_vel
+	if new_xz_vel_diff.length() <= _VELOCITY_DIFF_SNAP_THRESHOLD or not is_zero_approx(new_xz_vel_diff.normalized().angle() - direction.angle()):
+		final_vel = goal_vel
+		
+	return final_vel
 
 ## Get the character's desired move direction in the XZ plane for the given angle
 func get_move_direction(angle: float) -> Vector3:
@@ -52,24 +121,5 @@ func closest_to_zero_velocity() -> Vector3:
 func can_climb() -> bool:
 	return false
 
-func _physics_process(delta: float) -> void:
-	var is_on_floor = is_on_floor()
-	var last_velocity = velocity
-	var gravity = get_gravity()
-	var gravity_direction = gravity.normalized()
-	
-	# How much the character's velocity will change this physics frame
-	var velocity_delta = Vector3.ZERO
-	
-	if try_jump:
-		if is_on_floor or (try_climb and can_climb()):
-			# Jump velocity is specified this way to support different gravity directions
-			velocity_delta -= gravity_direction * jump_velocity * delta
-		else:
-			velocity_delta += gravity_direction * delta
-	elif can_climb():
-		pass
-	elif not is_on_floor:
-		velocity_delta += gravity_direction * delta
-		
-	velocity = last_velocity + velocity_delta
+## Returns whether or not the character is currently trying to move
+func is_trying_to_move() -> bool: return forward_scale != 0 && right_scale != 0
